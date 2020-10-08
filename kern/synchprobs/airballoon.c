a2a55eb4b3b7ffb2ce3f11d9bb565d64a103bab4
SHENG WANG <wangshengwaletr@gmail.com>
2020-10-07 18:13:40 -0700

    Solution to Assignment 3

--- kern/synchprobs/airballoon.c
+++ kern/synchprobs/airballoon.c
@@ -15,10 +15,10 @@ static int ropes_left = NROPES;
 
 /* Implement this! */
 
-volatile int hook_connect[NROPES];
-volatile int hooks_on_stakes[NROPES];
+volatile int hook_connect[NROPES];   		//1 means connect
+											//0 means non ropes connect with hook
+volatile int hooks_on_stakes[NROPES];		//hooks_on_stakes[4] = 5 means hook 4 connect with stake 5
 
-volatile int stakes_connect[NROPES];
 
 
 /* Synchronization primitives */
@@ -29,10 +29,17 @@ volatile int stakes_connect[NROPES];
 static struct lock* rope_lk[NROPES];
 static struct lock* ropeleft_lk;
 
+static struct lock* dandelion_lk;
+static struct cv* dandelion_cv;
+static struct lock* marigold_lk;
+static struct cv* marigold_cv;
+// static struct lock* flowerkiller_lk;
+// static struct cv* flowerkiller_cv;
 
 
-static struct lock* balloon_lk;
-static struct cv* airballon_cv;
+
+
+
 static struct lock* main_thread_lk;
 static struct cv* main_thread_cv;
 
@@ -76,6 +83,7 @@ dandelion(void *p, unsigned long arg) //prince
 		//we still hold the lock of rope_lk[rand_hooks], we need to remember to release it
 
 	    hook_connect[rand_hooks] = 0;      //we get the connected hook, diconnect it
+		kprintf("Dandelion severed rope %d\n", rand_hooks);
 
 		lock_release(rope_lk[rand_hooks]);
 
@@ -85,7 +93,7 @@ dandelion(void *p, unsigned long arg) //prince
 	    lock_release(ropeleft_lk);   //release the lock of num of left ropes
 
 
-		kprintf("Dandelion severed rope %d\n", rand_hooks);
+
 	    thread_yield();                //lets go to another thread
 		
 	}
@@ -95,7 +103,7 @@ dandelion(void *p, unsigned long arg) //prince
 
 
 
-	cv_signal(airballon_cv, balloon_lk);
+	cv_signal(dandelion_cv, dandelion_lk);		//airballoon finish, wakeup it, let it start again
 
 
 }
@@ -133,6 +141,7 @@ marigold(void *p, unsigned long arg)
 				if (hook_connect[i] == 1){              //we have already find the corresponding hook, check it still needs disconnect or it was done by D				*
 	                    								//this stake is connect with a hook																				*
 					hook_connect[i] = 0;                //disconnect it																								  	*
+					kprintf("Marigold severed rope %d from stake %d\n", i, random_stake);   //print the rope information and stake information							*
 					lock_release(rope_lk[i]);			//release the lock*************************************************<---**************<---************************
 																																									//  *
 																																									//	*
@@ -141,7 +150,7 @@ marigold(void *p, unsigned long arg)
 					lock_release(ropeleft_lk);			//release the lock																								*
 																																									//	*
 																																									//	*
-					kprintf("Marigold severed rope %d from stake %d\n", i, random_stake);   //print the rope information and stake information							*
+					
 																																									//	*
 					thread_yield();                     //go to another thread																							*
 					break;								//when come back, move out from this loop, we need to find another appropriate stake. 							*
@@ -163,6 +172,7 @@ marigold(void *p, unsigned long arg)
 	}
 	
 	kprintf("Marigold thread done\n");
+	cv_signal(marigold_cv, marigold_lk);
 
 }
 
@@ -227,6 +237,7 @@ flowerkiller(void *p, unsigned long arg)
 		
 	}
 	kprintf("Lord FlowerKiller thread done\n");
+
 }
 
 static
@@ -238,13 +249,39 @@ balloon(void *p, unsigned long arg)
 
 	kprintf("Balloon thread starting\n");
 
-	lock_acquire(balloon_lk);
-	cv_wait(airballon_cv, balloon_lk);
-	lock_release(balloon_lk);
+	//craete locks for each person
+	dandelion_lk = lock_create("dandelion_lk");         	//create the dandelion lock for dandelion_cv
+	dandelion_cv = cv_create("dandelion_cv");				//create the  dandelion_cv to transfer signal
+	marigold_lk = lock_create("marigold_lk");
+	marigold_cv = cv_create("marigold_cv");
+	// flowerkiller_lk = lock_create("flowerkiller_lk");
+	// flowerkiller_cv = cv_create("flowerkiller_cv");
+
+	lock_acquire(dandelion_lk);								//acquire the lock of 	dandelion_lk				
+	cv_wait(dandelion_cv, dandelion_lk);					//wait the dandelion finish--- Balloon freed
+	lock_release(dandelion_lk);
+
+	lock_acquire(marigold_lk);								//acquire the lock of 	marigold_lk				
+	cv_wait(marigold_cv, marigold_lk);						//wait the marigold finish--- Balloon freed
+	lock_release(marigold_lk);
+
+	// lock_acquire(flowerkiller_lk);							//acquire the lock of 	flowerkiller_lk				
+	// cv_wait(flowerkiller_cv, flowerkiller_lk);				//wait the flowerkiller finish--- Balloon freed
+	// lock_release(flowerkiller_lk);	
+
+	//delete locks for each person
+	lock_destroy(dandelion_lk);								//finsh dandelion thread, no need dandelion_lk
+    cv_destroy(dandelion_cv);								//delete dandelion_cv
+	lock_destroy(marigold_lk);
+	cv_destroy(marigold_cv);
+	// lock_destroy(flowerkiller_lk);
+	// cv_destroy(flowerkiller_cv);
+
+
 	kprintf("Balloon freed and Prince Dandelion escapes!\n");
 	kprintf("Balloon thread done\n");
 	
-	cv_signal(main_thread_cv, main_thread_lk);
+	cv_signal(main_thread_cv, main_thread_lk);  		//main thread finish, wakeup it, let it start again
 
 
 	/* Implement this function */
@@ -287,14 +324,14 @@ airballoon(int nargs, char **args)
 	i = 0;
 
 	for(i = 0; i < NROPES; i ++){
-	    hook_connect[i] = 1;        //at the beginning all the hooks are connected with ropes
-	    hooks_on_stakes[i] = i;      //at the beginning every hooks connect with corresponding stakes
+	    hook_connect[i] = 1;        							//at the beginning all the hooks are connected with ropes
+	    hooks_on_stakes[i] = i;      							//at the beginning every hooks connect with corresponding stakes
 	}
 
-	for(i = 0; i < NROPES; i++){
-	    rope_lk[i] = lock_create("rope_lk_" + i);
+	for(i = 0; i < NROPES; i++){ 
+	    rope_lk[i] = lock_create("rope_lk_" + i);            	//create the lock of each hook and ropes
 	}
-	ropeleft_lk = lock_create("ropeleft_lk");
+	ropeleft_lk = lock_create("ropeleft_lk");					//create the lock of num of left ropes
 
 
 	err = thread_fork("Marigold Thread",
@@ -327,34 +364,32 @@ panic:
 	
 done:
 
-	balloon_lk = lock_create("balloon_lk");
-
-	airballon_cv = cv_create("airballon_cv");
-
-	main_thread_lk = lock_create("main_thread_lk");
-
-	main_thread_cv = cv_create("main_thread_cv");
-
-
-
 	
-    lock_acquire(main_thread_lk);
-    cv_wait(main_thread_cv, main_thread_lk);
-    lock_release(main_thread_lk);
+
+	main_thread_lk = lock_create("main_thread_lk");		//create the main thread lock for main_thread_cv
+	main_thread_cv = cv_create("main_thread_cv");		//create the main_thread_cv to transfer signal back 
+
+
+    lock_acquire(main_thread_lk);						//lock main_thread_lk
+    cv_wait(main_thread_cv, main_thread_lk);			//wait for the main thread finish
+    lock_release(main_thread_lk);						//release the main_thread_lk
+
+
+	lock_destroy(main_thread_lk);						//finsh main thread, no nedd main_thread_lk
+    cv_destroy(main_thread_cv);							//delete main_thread_cv
+
     
 
 
-    for(i = 0; i < NROPES; i++){
+    for(i = 0; i < NROPES; i++){              			//destory the rope_lk[0~15]
         lock_destroy(rope_lk[i]);
     }
-    lock_destroy(balloon_lk);
-	lock_destroy(main_thread_lk);
+
+	lock_destroy(ropeleft_lk);							//destory ropeleft_lk
 
 
-    cv_destroy(airballon_cv);
-    cv_destroy(main_thread_cv);
-
-
+	ropes_left = NROPES;								//prepare for next run
+    
 
     kprintf("Main thread done\n");
     return 0;
